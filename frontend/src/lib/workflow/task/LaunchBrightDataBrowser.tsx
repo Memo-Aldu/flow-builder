9881fcad95a4704a8355a7589dcd919bcf4c10dc
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2025-05-24 21:41:53 -0400

    Uppdated ui to reflect on new browsers

---
+++ frontend/src/lib/workflow/task/LaunchBrightDataBrowser.tsx
@@ -0,0 +1,27 @@
+import { TaskParamType, TaskType } from "@/types/task";
+import { WorkflowTask } from "@/types/workflows";
+import { GlobeLockIcon, LucideProps } from "lucide-react";
+
+export const LaunchBrightDataBrowserTask = {
+    type: TaskType.LAUNCH_BRIGHT_DATA_BROWSER,
+    label: "Bright Data Browser",
+    icon: (props: LucideProps) => <GlobeLockIcon className="stroke-orange-400" {...props} />,
+    description: "Launch a Bright Data browser with automatic proxy rotation and captcha solving",
+    isEntryPoint: true,
+    inputs: [
+        {
+            name: "Website URL",
+            type: TaskParamType.STRING,
+            helperText: "eg: https://www.google.com",
+            required: true,
+            hideHandle: true,
+        }
+    ],
+    outputs: [
+        {
+            name: "Web Page",
+            type: TaskParamType.BROWSER_INSTANCE,
+        }
+    ],
+    credits: 10,
+} satisfies WorkflowTask
---
+++ frontend/src/lib/workflow/task/LaunchStandardBrowser.tsx
@@ -0,0 +1,27 @@
+import { TaskParamType, TaskType } from "@/types/task";
+import { WorkflowTask } from "@/types/workflows";
+import { GlobeIcon, LucideProps } from "lucide-react";
+
+export const LaunchStandardBrowserTask = {
+    type: TaskType.LAUNCH_STANDARD_BROWSER,
+    label: "Standard Browser",
+    icon: (props: LucideProps) => <GlobeIcon className="stroke-orange-400" {...props} />,
+    description: "Launch a standard browser and navigate to a specific URL",
+    isEntryPoint: true,
+    inputs: [
+        {
+            name: "Website URL",
+            type: TaskParamType.STRING,
+            helperText: "eg: https://www.google.com",
+            required: true,
+            hideHandle: true,
+        }
+    ],
+    outputs: [
+        {
+            name: "Web Page",
+            type: TaskParamType.BROWSER_INSTANCE,
+        }
+    ],
+    credits: 5,
+} satisfies WorkflowTask
