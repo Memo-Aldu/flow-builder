8bc442e966eb414259acf5bc7d2d724f43fb03bc
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2025-06-27 22:17:39 -0400

    Added Vercel analytcis

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -62,6 +63,7 @@ export default function RootLayout({
     >
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Analytics />
           <AppProvider>
             <main>{children}</main>
             <Toaster richColors />
